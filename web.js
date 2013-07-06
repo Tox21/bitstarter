e54fcea0b0cb60ff0265e95c1af56c39f7fafb3c
John Smith <example@stanford.edu>
2013-07-06 17:03:27 -0400

    Updated web.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var fs = require('fs');
-var buf = fs.readFilesyn('index.html');
+var buf = fs.readFileSync('index.html');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
